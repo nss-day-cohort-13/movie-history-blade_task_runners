c368ca77c1e31ecc1aa9416ddab01d079782bd96
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-03 13:37:01 -0500

    Watched/unwatched evaluation working

--- public/app/userCtrl.js
+++ public/app/userCtrl.js
@@ -1,7 +1,29 @@
 angular.module('blade')
-	.controller('UserCtrl', function($scope, movieFactory){
+	.controller('UserCtrl', function($scope, firebaseFactory){
 
-		$scope.userMovies = firebaseFactory.userInfo;
+		$scope.userMovies = [
+			{
+				title: 'Blade Runner',
+				year: '1982',
+				actors: 'Harrison Ford',
+				user_rating: 0,
+				watched: true
+			},
+			{
+				title: '8 1/2',
+				year: '1963',
+				actors: 'Marcello Mastroianni',
+				user_rating: 0,
+				watched: false
+			},
+			{
+				title: 'No',
+				year: '2013',
+				actors: 'Gael Garcia Bernal',
+				user_rating: 0,
+				watched: false
+			}
+		];
 
 		$scope.watchedValue = false;
 
