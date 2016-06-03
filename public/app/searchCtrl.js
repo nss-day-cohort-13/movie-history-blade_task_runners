8a68aa765d40aa3e0f623c6c15b689f39005cb86
Cory Tohill <corytohill@gmail.com>
2016-06-03 16:06:53 -0500

    added comments to searchCtrl.js

--- public/app/searchCtrl.js
+++ public/app/searchCtrl.js
@@ -1,19 +1,28 @@
 angular.module('blade')
   .controller('SearchCtrl', function($scope, movieFactory) {
-    let searchedMoviesList = null;
 
+    //performs search when search button is clicked
     $scope.performSearch = (userText) => {
+
+      //calls the searchMovies call from factory, passing in the userText
       movieFactory.searchMovies(userText)
+        //store the Search part of the response data in the scope
         .then(searchResults => $scope.searchedMoviesList = searchResults.data.Search)
+
         .then(searchedMoviesList => {
+          //loops over each movie in the Search response
           searchedMoviesList.forEach((movie) => {
+            //performs the factory call for each movie
             movieFactory.movieInfo(movie.imdbID)
-              .then(response => movie.Actors = response)
-          })
-        })
-    }
+              //adds the key "Actors" to each movieand gives it the correct value from the API
+              .then(response => movie.Actors = response);
+          });
+        });
+    };
 
     $scope.addMovieToWatchlist = (movie) => {
       console.log("test", movie);
-    }
-  })
+      //add movie to firebase under user account
+      //disable ADD TO WATCHLIST button
+    };
+  });
